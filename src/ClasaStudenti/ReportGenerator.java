187d11d381fa94370bee1e244ce7a14f059a1967
BeniMalan <malanbeniaminflorin@gmail.com>
2023-04-10 23:35:03 +0300

    tema curs 10

--- src/ClasaStudenti/ReportGenerator.java
+++ src/ClasaStudenti/ReportGenerator.java
@@ -15,19 +15,10 @@ public class ReportGenerator {
 
     public static void generateReport(List<StudentGrade> students) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("Files/grade-reports.out"));
-
         Clasroom classroom = new Clasroom(students);
-
-        // Find the student with the highest grade
         StudentGrade maxGrade = classroom.getMaxGrade2();
-
-        // Find the student with the lowest grade
         StudentGrade minGrade = classroom.getWorstGrade(maxGrade.getDiscipline());
-
-        // Calculate the average grade
         int averageGrade = classroom.getAverageGrade(maxGrade.getDiscipline());
-
-        // Write the report to the file
         writer.write("Cea mai mare nota: " + maxGrade + "\n");
         writer.write("Nota medie: " + averageGrade + "\n");
         writer.write("Cea mai mica nota: " + minGrade + "\n");
